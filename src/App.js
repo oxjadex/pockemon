0d1bf62cd60e76a00b447a2056edcb07276ddbf6
oxjadex <oharin765@gmail.com>
2024-07-15 10:48:18 +0900

    feat: 백그라운드 이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import PokemonList from "./components/PokemonList";
+import styled from "styled-components";
+import background from "./assets/background.gif";
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <PokemonList />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  background: url(${background}) no-repeat center center fixed;
+`;
 
 export default App;
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,24 +1,77 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 const PokemonList = () => {
   const [pokemonData, setPokemonData] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const allPokemonData = []
-            for (let i = 1; i <= 0; i++) {
-                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
-                const speciesResult = await axios.get(`https://pokeapi.co/api/v2/species/${i}`)
-                const koreanName = speciesResult.data.name.find(
-                    (name) => name.language.name === "ko"
-                )
-                allPokemonData.push({ ...response.data, koreanName: koreanName.name })
-            }
-            fetchData()
-        }, [])
-    return ();
-      
+  useEffect(() => {
+    const fetchData = async () => {
+      const requests = [];
+      for (let i = 1; i <= 30; i++) {
+        requests.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`));
+        requests.push(
+          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`)
+        );
+      }
+
+      const responses = await Promise.all(requests);
+      const allPokemonData = [];
+
+      for (let i = 0; i < responses.length; i += 2) {
+        const pokemonResponse = responses[i];
+        const speciesResponse = responses[i + 1];
+        const koreanName = speciesResponse.data.names.find(
+          (name) => name.language.name === "ko"
+        );
+
+        allPokemonData.push({
+          ...pokemonResponse.data,
+          korean_name: koreanName ? koreanName.name : "No name found",
+        });
+      }
+      setPokemonData(allPokemonData);
+    };
+    fetchData();
+  }, []);
+
+  const renderPokemonList = () => {
+    return pokemonData.map((pokemon) => (
+      <Container>
+        <PokemonContainer>
+          <div key={pokemon.id}>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <p>{pokemon.korean_name}</p>
+            <p>ID: ({pokemon.id})</p>
+          </div>
+        </PokemonContainer>
+      </Container>
+    ));
+  };
+
+  return <div>{renderPokemonList()}</div>;
 };
 
 export default PokemonList;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  text-align: center;
+`;
+
+const PokemonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  text-align: center;
+`;
